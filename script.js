e0e668e82a6ff026fdaa33d0859a2d6440c4ec22
Portfolio <taufiquzzaman0@gmail.com>
2024-11-01 22:13:20 +0600

    index added

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".input",{
+    strings: ["Passionate Programmer","Front-End and", "Back-End Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+var typed = new Typed(".input1",{
+    strings: ["Machine-Learning Enthusiast"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
